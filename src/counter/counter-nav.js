eda9d7ae61d669162d47bed69308187145280cd4
Noureddine ABABAR <noureddineababar1@gmail.com>
2021-11-02 12:02:59 +0100

    init

---
+++ src/counter/counter-nav.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+
+import { useDispatch } from "react-redux"
+import {
+  increase,
+  decrease,
+  reset,
+} from "../redux"
+
+export const CounterNav = () => {
+  const dispatch = useDispatch()
+  const [val, setVal] = useState(0)
+  return (
+    <div className="counter-nav">
+      <button
+        onClick={() => {
+          dispatch(decrease())
+        }}
+      >
+        Decrease
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(reset())
+        }}
+      >
+        Reset
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(increase())
+        }}
+      >
+        Increase
+      </button>
+      <button>
+        Increase odd number
+      </button>
+      <button>
+        Increase with specific value
+      </button>
+      <input
+        type="number"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default CounterNav
